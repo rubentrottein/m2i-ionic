254dd9f2b516430993ac9f8317a41d5fbe203ad3
rubentrottein <38758157+rubentrottein@users.noreply.github.com>
2024-07-12 13:59:44 +0200

    Interrogation de l'API locale

--- ioniccrew2/src/app/scss/scss.page.ts
+++ ioniccrew2/src/app/scss/scss.page.ts
@@ -1,21 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+interface Article {
+  // Define the properties of an Article based on your data model
+  title: string;
+  img : "https://picsum.photos/500";
+}
 
 @Component({
   selector: 'app-scss',
   templateUrl: './scss.page.html',
   styleUrls: ['./scss.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ExploreContainerComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ExploreContainerComponent, NgFor]
 })
 export class ScssPage implements OnInit {
 
-  constructor() { }
+  articles: Article[] = [];
+
+  constructor() {}
 
   ngOnInit() {
+    this.loadArticles();
   }
 
+  async loadArticles() {
+    try {
+      this.articles = await this.fetchJson<Article>("http://localhost:3999/api/school/articles");
+      console.log(await this.fetchJson<Article>("http://localhost:3999/api/school/articles"))
+    } catch (error) {
+      console.error('Error fetching articles:', error);
+    }
+  }
+
+  async fetchJson<T>(url: string): Promise<T[]> {
+    return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Forsooth, a scourge upon our fetch quest: ' + response.statusText);
+      }
+      return response.json() as Promise<T[]>;
+    })
+    .then(data => {
+      return data;
+    })
+  }
 }
