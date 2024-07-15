1f0b2224f9507bfe85a1680c44953fe74cde35f1
rubentrottein <ruben.trottein@gmail.com>
2024-07-15 15:52:58 +0200

    Ajout du projet what-to-do

---
+++ what-to-do/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'What To-do',
+  webDir: 'www'
+};
+
+export default config;
